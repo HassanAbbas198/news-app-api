1a21caf7c7410f60c0f7aaeb61ae5db95ed50c72
Hassan <hassan.abbascs@gmail.com>
2021-01-02 11:23:24 +0200

    added environment variables

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+const ENV = require('./config/config');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const { PORT } = ENV;
 
 app.use(bodyParser.json());
 
